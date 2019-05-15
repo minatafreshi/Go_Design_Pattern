9628bbee33457bc747571fdfd4395d9a2d412165
mina tafreshi <info.minatafreshi@gmail.com>
2019-05-15 12:42:05 +0430

    Object Pool added

---
+++ .history/Creational/Object_Pool_20190515124107.go
@@ -0,0 +1,48 @@
+package Creational
+
+import (
+	"sync"
+)
+
+// Pool represents the pool of objects to use.
+type Pool struct {
+	sync.Mutex
+	inuse     []interface{}
+	available []interface{}
+	new       func() interface{}
+}
+
+// NewPool creates a new pool.
+func NewPool(new func() interface{}) *Pool {
+	return &Pool{new: new}
+}
+
+// Acquire acquires a new PoolObject to use from the pool.
+// Here acquire creates a new instance of a PoolObject if none available.
+func (p *Pool) Acquire() interface{} {
+	p.Lock()
+	var object interface{}
+	if len(p.available) != 0 {
+		object = p.available[0]
+		p.available = append(p.available[:0], p.available[1:]...)
+		p.inuse = append(p.inuse, object)
+	} else {
+		object = p.new()
+		p.inuse = append(p.inuse, object)
+	}
+	p.Unlock()
+	return object
+}
+
+// Release releases a PoolObject back to the Pool.
+func (p *Pool) Release(object interface{}) {
+	p.Lock()
+	p.available = append(p.available, object)
+	for i, v := range p.inuse {
+		if v == object {
+			p.inuse = append(p.inuse[:i], p.inuse[i+1:]...)
+			break
+		}
+	}
+	p.Unlock()
+}
---
+++ .history/Creational/Object_Pool_20190515124120.go
@@ -0,0 +1,44 @@
+package Creational
+
+import (
+	"sync"
+)
+
+type Pool struct {
+	sync.Mutex
+	inuse     []interface{}
+	available []interface{}
+	new       func() interface{}
+}
+
+func NewPool(new func() interface{}) *Pool {
+	return &Pool{new: new}
+}
+
+func (p *Pool) Acquire() interface{} {
+	p.Lock()
+	var object interface{}
+	if len(p.available) != 0 {
+		object = p.available[0]
+		p.available = append(p.available[:0], p.available[1:]...)
+		p.inuse = append(p.inuse, object)
+	} else {
+		object = p.new()
+		p.inuse = append(p.inuse, object)
+	}
+	p.Unlock()
+	return object
+}
+
+// Release releases a PoolObject back to the Pool.
+func (p *Pool) Release(object interface{}) {
+	p.Lock()
+	p.available = append(p.available, object)
+	for i, v := range p.inuse {
+		if v == object {
+			p.inuse = append(p.inuse[:i], p.inuse[i+1:]...)
+			break
+		}
+	}
+	p.Unlock()
+}
