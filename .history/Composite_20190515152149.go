80d0b59639f03aecc24d1dd34e959dc68f0e1b46
mina tafreshi <info.minatafreshi@gmail.com>
2019-05-15 15:23:07 +0430

    Composite added

---
+++ .history/Composite_20190515152149.go
@@ -0,0 +1,41 @@
+package Structural
+
+import "fmt"
+
+type Component interface {
+	Traverse()
+}
+
+type Leaf struct {
+	value int
+}
+
+func NewLeaf(value int) *Leaf {
+	return &Leaf{value}
+}
+
+func (l *Leaf) Traverse() {
+	fmt.Printf("%v  ", l.value)
+}
+
+// Composite describes a composite of components.
+type Composite struct {
+	children []Component
+}
+
+// NewComposite creates a new composite.
+func NewComposite() *Composite {
+	return &Composite{make([]Component, 0)}
+}
+
+// Add adds a new component to the composite.
+func (c *Composite) Add(component Component) {
+	c.children = append(c.children, component)
+}
+
+// Traverse traverses the composites children.
+func (c *Composite) Traverse() {
+	for i := 0; i < len(c.children); i++ {
+		c.children[i].Traverse()
+	}
+}
