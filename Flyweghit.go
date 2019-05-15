9cf1575557d1f1e4194913ce018561f925eb1130
mina tafreshi <info.minatafreshi@gmail.com>
2019-05-15 15:13:37 +0430

    Flyweghit added

---
+++ Flyweghit.go
@@ -0,0 +1,29 @@
+package Structural
+
+
+type Flyweight struct {
+	Name string
+}
+
+func NewFlyweight(name string) *Flyweight {
+	return &Flyweight{name}
+}
+
+// FlyweightFactory is a factory for creating and storing flyweights.
+type FlyweightFactory struct {
+	pool map[string]*Flyweight
+}
+
+func NewFlyweightFactory() *FlyweightFactory {
+	return &FlyweightFactory{pool: make(map[string]*Flyweight)}
+}
+
+
+func (f *FlyweightFactory) GetFlyweight(name string) *Flyweight {
+	flyweight, okay := f.pool[name]
+	if !okay {
+		flyweight = NewFlyweight(name)
+		f.pool[name] = flyweight
+	}
+	return flyweight
+}
