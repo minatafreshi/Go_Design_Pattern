61acfbfd23b1d5cc91eded23eb8827e7e9e5a5e1
mina tafreshi <info.minatafreshi@gmail.com>
2019-05-15 14:58:00 +0430

    Decorator added

---
+++ .history/Decorator_20190515145647.go
@@ -0,0 +1,19 @@
+package structural
+
+import (
+	"log"
+)
+type TaskFunc func(int) int
+
+// LogDecorate decorates a task functions execution with some logging.
+func LogDecorate(fn TaskFunc) TaskFunc {
+	return func(taskID int) int {
+		log.Printf("Starting task with ID %v.\n", taskID)
+
+		result := fn(taskID)
+
+		log.Printf("Task with ID %v completed with the result %v.\n", taskID, result)
+
+		return result
+	}
+}
