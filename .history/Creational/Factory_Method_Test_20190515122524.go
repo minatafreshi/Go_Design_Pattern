2ed3de8e31e2424890abc23b45b82387eff7287e
mina tafreshi <info.minatafreshi@gmail.com>
2019-05-15 12:36:21 +0430

    Factrory Method test added

---
+++ .history/Creational/Factory_Method_Test_20190515122524.go
@@ -0,0 +1,64 @@
+package creational
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewStooge_WhenStoogeTypeIsLarry_ReturnsLarry(t *testing.T) {
+	stooge := NewStooge(Larry)
+	assert.NotNil(t, stooge)
+	_, ok := stooge.(*larry)
+	assert.True(t, ok)
+}
+
+func TestNewStooge_WhenStoogeTypeIsMoe_ReturnsMoe(t *testing.T) {
+	stooge := NewStooge(Moe)
+	assert.NotNil(t, stooge)
+	_, ok := stooge.(*moe)
+	assert.True(t, ok)
+}
+
+func TestNewStooge_WhenStoogeTypeIsCurly_ReturnsCurly(t *testing.T) {
+	stooge := NewStooge(Curly)
+	assert.NotNil(t, stooge)
+	_, ok := stooge.(*curly)
+	assert.True(t, ok)
+}
+
+func TestNewStooge_WhenStoogeTypeIsNotRecognised_ReturnsNil(t *testing.T) {
+	stooge := NewStooge(10)
+	assert.Nil(t, stooge)
+}
+
+func TestSlapStick_WhenNewStoogeIsLarry_PrintsLarryMessage(t *testing.T) {
+	bufferOutputWriter := outputWriter
+	outputWriter = new(bytes.Buffer)
+	defer func() { outputWriter = bufferOutputWriter }()
+
+	stooge := NewStooge(Larry)
+	stooge.SlapStick()
+	assert.Equal(t, "Larry: Poke eyes\n", outputWriter.(*bytes.Buffer).String())
+}
+
+func TestSlapStick_WhenNewStoogeIsMoe_PrintsMoeMessage(t *testing.T) {
+	bufferOutputWriter := outputWriter
+	outputWriter = new(bytes.Buffer)
+	defer func() { outputWriter = bufferOutputWriter }()
+
+	stooge := NewStooge(Moe)
+	stooge.SlapStick()
+	assert.Equal(t, "Moe: Slap head\n", outputWriter.(*bytes.Buffer).String())
+}
+
+func TestSlapStick_WhenNewStoogeIsCurly_PrintsCurlyMessage(t *testing.T) {
+	bufferOutputWriter := outputWriter
+	outputWriter = new(bytes.Buffer)
+	defer func() { outputWriter = bufferOutputWriter }()
+
+	stooge := NewStooge(Curly)
+	stooge.SlapStick()
+	assert.Equal(t, "Curly: Suffer abuse\n", outputWriter.(*bytes.Buffer).String())
+}
