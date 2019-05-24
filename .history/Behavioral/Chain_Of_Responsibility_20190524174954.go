86e1fdf06dbf8b77aad6b0964670bd4d6bfce472
mina tafreshi <info.minatafreshi@gmail.com>
2019-05-24 17:51:17 +0430

    Chain of responsibility added

---
+++ .history/Behavioral/Chain_Of_Responsibility_20190524174954.go
@@ -0,0 +1,26 @@
+package Behavioral
+
+import (
+	"strconv"
+)
+
+type Handler interface {
+	Handle(handleID int) string
+}
+
+type handler struct {
+	name     string
+	next     Handler
+	handleID int
+}
+
+func NewHandler(name string, next Handler, handleID int) Handler {
+	return &handler{name, next, handleID}
+}
+
+func (h *handler) Handle(handleID int) string {
+	if h.handleID == handleID {
+		return h.name + " handled " + strconv.Itoa(handleID)
+	}
+	return h.next.Handle(handleID)
+}
