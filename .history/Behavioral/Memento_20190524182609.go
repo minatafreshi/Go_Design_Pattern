58c95e7c73ee872818cd3bc6917a9cf0b9d42072
mina tafreshi <info.minatafreshi@gmail.com>
2019-05-24 18:26:39 +0430

    Memento added

---
+++ .history/Behavioral/Memento_20190524182609.go
@@ -0,0 +1,37 @@
+package Behavioral
+
+type Memento struct {
+	state int
+}
+
+func NewMemento(value int) *Memento {
+	return &Memento{value}
+}
+
+type Number struct {
+	value int
+}
+
+func NewNumber(value int) *Number {
+	return &Number{value}
+}
+
+func (n *Number) Dubble() {
+	n.value = 2 * n.value
+}
+
+func (n *Number) Half() {
+	n.value = n.value / 2
+}
+
+func (n *Number) Value() int {
+	return n.value
+}
+
+func (n *Number) CreateMemento() *Memento {
+	return NewMemento(n.value)
+}
+
+func (n *Number) ReinstateMemento(memento *Memento) {
+	n.value = memento.state
+}
